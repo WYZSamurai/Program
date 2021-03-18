7f18da46d6d1317d1fb3808b7d8e11d5b6dff0dd
Samurai <963513255@qq.com>
2021-03-18 13:11:34 +0800

    upload "hanoi"

--- C++/stack/hanoi.cpp
+++ C++/stack/hanoi.cpp
@@ -2,10 +2,31 @@
 #include <stack>
 using namespace std;
 
-void hanoi(char a){}
+void move(int a, char x, char y)
+{
+}
+
+void hanoi(int n, char A, char B, char C)
+{
+    if (n <= 1)
+    {
+        printf("1 move %c to %c\n", A, C);
+        return;
+    }
+    hanoi(n - 1, A, C, B);
+    printf("%d move %c to %c \n", n, A, C);
+    hanoi(n - 1, B, A, C);
+}
 
 int main()
 {
+    char x, y, z;
+    x = 'X';
+    y = 'Y';
+    z = 'Z';
+    int n;
+    cin >> n;
+    hanoi(n, x, y, z);
     system("pause");
     return 0;
 }
