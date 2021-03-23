0266bd82634c10a67f9ada4ae26988b6940f7669
Samurai <963513255@qq.com>
2021-03-23 10:15:03 +0800

    修改435题

--- C++/Algorithms/greedy algorithm/435.cpp
+++ C++/Algorithms/greedy algorithm/435.cpp
@@ -8,11 +8,14 @@ int func(vector<vector<int>> &intervals)
     if (intervals.size() <= 1)
         return 0;
     sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+        //return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
         return a[1] < b[1];
     });
     int n = intervals.size();
     int m = 0;
-    /*for (int i = 0; i < n - 1; i++)
+
+    /*
+    for (int i = 0; i < n - 1; i++)
     {
         if (intervals[i][0] == intervals[i + 1][0])
         {
@@ -29,7 +32,7 @@ int func(vector<vector<int>> &intervals)
             if (intervals[i][1] > intervals[i + 1][0])
             {
                 m++;
-                if ((intervals[i][1] - intervals[i][0]) > (intervals[i + 1][1] - intervals[i + 1][0]))
+                if (intervals[i][1] > intervals[i + 1][1])
                 {
                     n--;
                     intervals.erase(intervals.begin() + i);
@@ -44,6 +47,7 @@ int func(vector<vector<int>> &intervals)
             }
         }
     }*/
+
     int right = intervals[0][1];
     for (int i = 1; i < n; i++)
     {
@@ -72,16 +76,6 @@ int main()
         v[i].push_back(b);
     }
     int m = func(v);
-    N = v.size();
-    cout << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << v[i][j] << ' ';
-        }
-        cout << endl;
-    }
     cout << "Total = " << m << endl;
 
     system("pause");
