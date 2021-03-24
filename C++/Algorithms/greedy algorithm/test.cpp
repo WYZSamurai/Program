e1ecd1140daf461d553cbcdec4a204f18b88720e
Samurai <963513255@qq.com>
2021-03-24 17:08:40 +0800

    add Java

--- C++/Algorithms/greedy algorithm/test.cpp
+++ C++/Algorithms/greedy algorithm/test.cpp
@@ -1,18 +1,8 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
-    int m, n;
-    cin >> m >> n;
-    cout << m << ' ' << n << endl;
-    vector<vector<int>> v(2);
-    v[0].push_back(m);
-    v[0].push_back(n);
-    cout << v[0].size() << endl;
-
     system("pause");
     return 0;
 }
