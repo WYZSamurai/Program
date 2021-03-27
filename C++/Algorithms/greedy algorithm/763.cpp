491a8277316cf8bc71caacff278aabe956c32d73
Samurai <963513255@qq.com>
2021-03-27 10:16:38 +0800

    add 763

---
+++ C++/Algorithms/greedy algorithm/763.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> partitionLabels(string S)
+    {
+        
+
+        vector<int> a;
+        return a;
+    }
+};
+
+int main()
+{
+    string S;
+    cin >> S;
+    Solution a;
+    a.partitionLabels(S);
+
+    system("pause");
+    return 0;
+}
