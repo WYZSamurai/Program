07667507fc1dfecb11d1afe5de99bfa15a0dbd1f
Samurai <963513255@qq.com>
2021-04-10 17:34:06 +0800

    Maven

--- JAVA/Test.java
+++ JAVA/Test.java
@@ -1,8 +1,15 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Fixed!");
+        System.out.println("Fixd!");
     }
 }
 
 class Dog {
+    int Age;
+    String name;
+
+    public Dog() {
+    }
+
+    
 }
---
+++ JAVA/xsx/src/main/java/ddsds/App.java
@@ -0,0 +1,11 @@
+package ddsds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Wld!");
+    }
+}
