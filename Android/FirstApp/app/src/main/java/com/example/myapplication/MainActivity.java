71705bb652d9d376c9335263e70b2c03369f0c8e
Samurai <963513255@qq.com>
2021-04-14 10:39:34 +0800

    add MainActivity

--- Android/FirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/FirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(v -> Toast.makeText(MainActivity.this, "clicked Button1",Toast.LENGTH_LONG).show());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.setting){
+            Toast.makeText(this,"clicked Setting",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
