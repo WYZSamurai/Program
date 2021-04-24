bbb80f374a0864a2ad6ef31b73e9593ff7c178fe
Samurai <963513255@qq.com>
2021-04-24 12:11:29 +0800

    cycle life test

--- Android/LifeCycleTest/app/src/main/java/com/example/lifecycletest/Normal.java
+++ Android/LifeCycleTest/app/src/main/java/com/example/lifecycletest/Normal.java
@@ -2,7 +2,9 @@ package com.example.lifecycletest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Normal extends AppCompatActivity {
 
